a033d696ac95f749c42d3d15ec61b092d06cb973
G Roshan Lal <groshanlal@gmail.com>
2022-04-28 14:17:53 -0700

    Move explainer code to dedicated class, collect more solutions per stage and then choose good rules during set cover

---
+++ lib/rule_builder.py
@@ -0,0 +1,227 @@
+class RuleBuilder:
+	def __init__(self, random_forest, candidate_rules, labels, tree_weight, tree_bias):
+		self.random_forest = random_forest
+		self.candidate_rules = candidate_rules
+		self.solution_rules = []
+		self.tree_weight = tree_weight
+		self.tree_bias = tree_bias
+		self.labels = labels
+		self.positives = []
+		for i in range(len(self.labels)):
+			if(self.labels[i] == 1):
+				self.positives.append(i)
+
+		print()
+		print("Positives: " + str(len(self.positives)))
+
+		print()
+		print("Rules from trees")
+		print(str(len(self.candidate_rules)) + " candidate rules")
+		
+		self.deduplicate()
+		
+		print("Deduping...")
+		print(str(len(self.candidate_rules)) + " candidate rules")
+		
+		self.generate_solutions()
+
+		print()
+		print("Set Cover")
+		self.pick_top_rules()
+
+	def pick_top_rules(self):
+		self.positives_to_explain = self.positives		
+		
+		positive_support = []
+		for r in self.solution_rules:
+			positive_support.append(list(set(self.positives).intersection(set(r.decision_support))))
+
+		top_rule_indices = []
+		while(len(self.positives_to_explain) > 0):
+			#choose biggest one
+			index = 0
+			for i in range(len(positive_support)):
+				if(len(positive_support[i]) > 0):
+					if(len(positive_support[i]) > len(positive_support[index])):
+						index = i
+					else:
+						if(len(positive_support[i]) == len(positive_support[index])):
+							if(len(self.solution_rules[i].decision_rule) < len(self.solution_rules[index].decision_rule)):
+								index = i
+
+			top_rule_indices.append(index)
+			self.positives_to_explain = list(set(self.positives_to_explain).difference(set(positive_support[index])))
+
+			#remove support
+			for i in range(len(positive_support)):
+				if(index != i):
+					positive_support[i] = list(set(positive_support[i]).difference(set(positive_support[index])))
+			positive_support[index] = []
+		
+		solutions = []
+		for i in top_rule_indices:
+			solutions.append(self.solution_rules[i])
+		self.solution_rules = solutions
+		print(len(self.solution_rules))
+
+
+	def generate_solutions(self):
+		print()
+		print("Running Apriori")
+
+		self.positives_to_explain = self.positives
+		stage = 0
+		while((len(self.positives_to_explain) > 0) and (stage <= self.random_forest.get_num_trees())):
+			print()
+			print("Rules from " + str(stage + 1) + " trees")
+		
+			new_candidates = []
+			if(stage == 0):
+				for r in self.candidate_rules:
+					if(self.filter_candidates(r) == True):
+						new_candidates.append(r)
+			else:
+				for i in range(len(self.candidate_rules)):
+					for j in range(i, len(self.candidate_rules)):
+						r = self.candidate_rules[i].join(self.candidate_rules[j])
+						if((r is not None) and (self.filter_candidates(r) == True)):
+								new_candidates.append(r)
+			self.candidate_rules = new_candidates
+
+			self.prune()
+
+			print("Pruning Candidates")
+			print(str(len(self.candidate_rules)) + " candidates")
+			print(str(len(self.solution_rules)) + " solutions")
+
+			self.deduplicate()
+			
+			print("Deduping...")
+			print(str(len(self.candidate_rules)) + " candidates")
+			print(str(len(self.solution_rules)) + " solutions")
+			
+			fidelity, fidelity_positives, fidelity_negatives = self.get_fidelity()
+			
+			print("Fidelity")
+			print("Total: " + str(fidelity) + ", Positive: " + str(fidelity_positives) + ", Negative: " + str(fidelity_negatives))
+			
+			print("Unexplained Positives")
+			print(len(self.positives_to_explain))
+
+			stage = stage + 1
+
+
+	def filter_candidates(self, rule):
+		decision_value = []
+		for data_index in rule.decision_support:
+			decision_value.append(self.labels[data_index])
+
+		decision_rule_precision = self.get_precision(rule, scores = decision_value)
+		solution_is_possible = self.check_coverage(rule, scores = decision_value)
+
+		if(decision_rule_precision >= 0.95):
+			self.solution_rules.append(rule)
+			self.positives_to_explain = list(set(self.positives_to_explain).difference(set(rule.decision_support)))
+		else:
+			if(solution_is_possible):
+				return True
+		return False
+
+
+	def get_fidelity(self, use_top = None):
+		if(use_top is None):
+			use_top = len(self.solution_rules)
+
+		support = []
+		for i in range(use_top):
+			r = self.solution_rules[i]
+			support = support + r.decision_support
+		support = list(set(support))
+
+		y_pred_rules = [0]*len(self.labels)
+		for s in support:
+			y_pred_rules[s] = 1 
+		
+		positives = 0
+		fidelity_positives = 0
+		negatives = 0
+		fidelity_negatives = 0
+		for i in range(len(self.labels)):
+			if(self.labels[i] == 1):
+				positives = positives + 1
+				if(y_pred_rules[i] == self.labels[i]):
+					fidelity_positives = fidelity_positives + 1
+			if(self.labels[i] == 0):
+				negatives = negatives + 1
+				if(y_pred_rules[i] == self.labels[i]):
+					fidelity_negatives = fidelity_negatives + 1
+
+		return (fidelity_positives + fidelity_negatives) / (positives + negatives), fidelity_positives / positives, fidelity_negatives / negatives
+
+
+	def prune(self):
+		pruned_candidates = []
+		for i in range(len(self.candidate_rules)):
+			rule = self.candidate_rules[i]
+
+			decision_value = []
+			for data_index in rule.decision_support:
+				decision_value.append(self.labels[data_index])
+
+			solution_is_possible = self.check_coverage(rule, scores = decision_value)
+			
+			if(solution_is_possible):
+				pruned_candidates.append(rule)
+		self.candidate_rules = pruned_candidates
+		return
+
+	def get_precision(self, rule, scores = None):
+		min_score, max_score = self.random_forest.get_rule_score(rule.decision_rule)
+
+		decision_rule_precision = 0.00
+		if(min_score*self.tree_weight + self.tree_bias >= 0):
+			decision_rule_precision = 1.00
+		
+		if(scores is not None):
+			decision_rule_precision = sum(scores)/len(scores)
+
+		return decision_rule_precision
+		
+	def check_coverage(self, rule, scores = None):
+		min_score, max_score = self.random_forest.get_rule_score(rule.decision_rule)
+
+		solution_is_possible = True 
+		if(max_score*self.tree_weight + self.tree_bias < 0):
+			solution_is_possible = False
+				
+		if(scores is not None):
+			if(max(scores) == 0):
+				solution_is_possible = False
+		
+			decision_support_positive = list(set(rule.decision_support).intersection(set(self.positives_to_explain)))
+		
+			if(len(decision_support_positive) == 0):
+				solution_is_possible = False
+
+		return solution_is_possible
+
+
+	def deduplicate(self):		
+		def dedup(rules):		
+			dedup_rules = []
+			dedup_decision_rules = []
+			for i in range(len(rules)):
+				if(rules[i].decision_rule not in dedup_decision_rules):
+					dedup_rules.append(rules[i])
+					dedup_decision_rules.append(rules[i].decision_rule)
+				else:
+					index = dedup_decision_rules.index(rules[i].decision_rule)
+					dedup_rules[index].identity = list(set(dedup_rules[index].identity).union(set(rules[i].identity)))
+
+			for i in range(len(dedup_rules)):
+				dedup_rules[i].create_identity_map()
+			return dedup_rules
+
+		self.candidate_rules = dedup(self.candidate_rules)
+		self.solution_rules = dedup(self.solution_rules)
+		return
--- train.py
+++ train.py
@@ -3,313 +3,71 @@ from lib.trainer import Trainer
 from lib.adapter import ScikitTreeAdapter, ScikitForestAdapter
 from sklearn.tree import export_text, plot_tree
 from lib.rule import Rule
+from lib.rule_builder import RuleBuilder
 import numpy as np
 
-def pipeline(training_data_path="data/train.csv", testing_data_path="data/test.csv", num_estimators=10, fidelity_threshold=0.95):
-  print("XGBoost Model")
-  trainer = Trainer(training_data_loc=training_data_path, 
-        testing_data_loc=testing_data_path,
-        scikit_model=GradientBoostingClassifier(n_estimators=num_estimators))
-  print("Accuracy")
-  accuracy, auc = trainer.evaluate_model()
-  print(accuracy) 
-  print("AUC")
-  print(auc)
+print("XGBoost Model")
+trainer = Trainer(training_data_loc="data/train.csv", 
+      testing_data_loc="data/test.csv",
+      scikit_model=GradientBoostingClassifier(n_estimators=10))
+print("Accuracy")
+accuracy, auc = trainer.evaluate_model()
+print(accuracy) 
+print("AUC")
+print(auc)
 
-  y_pred = trainer.model.predict(trainer.x_train)
-  if(sum(y_pred) == 0):
-    print("Model doen't learn any positive")
+y_pred = trainer.model.predict(trainer.x_train)
+if(sum(y_pred) == 0):
+  print("Model doen't learn any positive")
 
-  # Calculating bias
-  positive = sum(trainer.y_train)
-  negative = len(trainer.y_train) - sum(trainer.y_train)
-  log_odds = np.log(positive/negative)
-  bias = log_odds
+# Calculating bias
+positive = sum(trainer.y_train)
+negative = len(trainer.y_train) - sum(trainer.y_train)
+log_odds = np.log(positive/negative)
+bias = log_odds
 
-  # Weights of Trees
-  learning_rate = trainer.model.get_params()['learning_rate']
+# Weights of Trees
+learning_rate = trainer.model.get_params()['learning_rate']
 
-  # Get candidate rules
-  random_forest = ScikitForestAdapter(trainer.model, trainer.feature_names).random_forest
-  num_trees = random_forest.get_num_trees()
-  print(str(num_trees) + " trees")
-  candidates = random_forest.get_rules(data=trainer.x_train, feature_names=trainer.feature_names)
+# Get candidate rules
+random_forest = ScikitForestAdapter(trainer.model, trainer.feature_names).random_forest
+num_trees = random_forest.get_num_trees()
+print(str(num_trees) + " trees")
+candidates = random_forest.get_rules(data=trainer.x_train, feature_names=trainer.feature_names)
 
-  def merge(candidates):
-    merged_candidates = []
-    merged_candidates_rule = []
-    for i in range(len(candidates)):
-      if(candidates[i].decision_rule not in merged_candidates_rule):
-        merged_candidates.append(candidates[i])
-        merged_candidates_rule.append(candidates[i].decision_rule)
-      else:
-        index = merged_candidates_rule.index(candidates[i].decision_rule)
-        merged_candidates[index].identity = list(set(merged_candidates[index].identity).union(set(candidates[i].identity)))
+explainer = RuleBuilder(random_forest = random_forest, candidate_rules = candidates, labels = y_pred, 
+  tree_weight = learning_rate, tree_bias = log_odds)
 
-    for i in range(len(merged_candidates)):
-      merged_candidates[i].create_identity_map()
+solutions = explainer.solution_rules
+positive_points = explainer.positives
 
-    return merged_candidates
-
-  def get_fidelity(rules, y_pred):
-    support = []
-    for r in rules:
-      support = support + r.decision_support
-    support = list(set(support))
-
-    y_pred_dup = [0]*len(y_pred)
-    for s in support:
-      y_pred_dup[s] = 1 
-    
-    positives = 0
-    fidelity_positives = 0
-    negatives = 0
-    fidelity_negatives = 0
-    for i in range(len(y_pred)):
-      if(y_pred[i] == 1):
-        positives = positives + 1
-        if(y_pred_dup[i] == y_pred[i]):
-          fidelity_positives = fidelity_positives + 1
-      if(y_pred[i] == 0):
-        negatives = negatives + 1
-        if(y_pred_dup[i] == y_pred[i]):
-          fidelity_negatives = fidelity_negatives + 1
-
-    return (fidelity_positives + fidelity_negatives) / (positives + negatives), fidelity_positives / positives, fidelity_negatives / negatives
-
-  print()
-  print("Rules from trees")
-  print()
-  print(str(len(candidates)) + " candidates")
-  print("Merging candidates...")
-  candidates = merge(candidates)
-  print(str(len(candidates)) + " candidates")
-
-  positive_points = []
-  for i in range(len(y_pred)):
-    if(y_pred[i] == 1):
-      positive_points.append(i)
-  assert(len(positive_points) == sum(y_pred))
-
-  # Threshold on score is 0, sigma(0) = 0.5
-  old_candidates = candidates
-  candidates = []
-  solutions = []
-  explained_positive_points = []
-  for r in old_candidates:
-    min_score, max_score = random_forest.get_rule_score(r.decision_rule)
-
-    decision_rule_precision = 0.00
-    if(min_score*learning_rate + bias >= 0):
-      decision_rule_precision = 1.00
-
-    solution_is_possible = True 
-    if(max_score*learning_rate + bias < 0):
-      solution_is_possible = False
-    
-    decision_value = []
-    for data_index in r.decision_support:
-      decision_value.append(y_pred[data_index])
-    decision_rule_precision = sum(decision_value)/len(decision_value)
-    
-    if(max(decision_value) == 0):
-      solution_is_possible = False
-    
-    decision_support_positive = list(set(r.decision_support).intersection(set(positive_points)))
-    
-    if(len(list(set(decision_support_positive).difference(set(explained_positive_points)))) == 0):
-      solution_is_possible = False
-    
-    if(solution_is_possible):
-      if(decision_rule_precision >= fidelity_threshold):
-        solutions.append(r)
-        explained_positive_points = list(set(explained_positive_points).union(set(decision_support_positive)))
-      else:
-        candidates.append(r)
-
-  print()
-  print("Running Apriori")
-
-  print()
-  print("Stage 0")
-  print("Pruning Candidates")
-  pruned_candidates = []
-  for i in range(len(candidates)):
-    r = candidates[i]
-    decision_support_positive = list(set(r.decision_support).intersection(set(positive_points)))
-    
-    solution_is_possible = True
-    if(len(list(set(decision_support_positive).difference(set(explained_positive_points)))) == 0):
-      solution_is_possible = False
-    
-    if(solution_is_possible):
-      pruned_candidates.append(r)
-  candidates = pruned_candidates
-
-  print()
-  print(str(len(candidates)) + " candidates")
-  print(str(len(solutions)) + " solutions")
-  print("Merging candidates and solutions...")
-  candidates = merge(candidates)
-  solutions = merge(solutions)
-  print(str(len(candidates)) + " candidates")
-  print(str(len(solutions)) + " solutions")
-  print()
-  fidelity, fidelity_positives, fidelity_negatives = get_fidelity(solutions, y_pred)
-  print("Fidelity")
+print()
+print("Solutions:")
+coverage = []
+positive_coverage = []
+for i in range(len(solutions)):
+  r = solutions[i]
+  print("Rule " + str(i + 1) + ": " + str(r.decision_rule))
+  positive_support = list(set(positive_points).intersection(set(r.decision_support)))
+  print("Precision: " + str(len(positive_support)/len(r.decision_support)))
+  print("Recall: " + str(len(positive_support)/len(positive_points)))
+  print("Coverage: " + str(len(r.decision_support)/len(y_pred)))
+  coverage = list(set(coverage).union(r.decision_support))
+  positive_coverage = list(set(positive_coverage).union(positive_support))
+  print("Cumulative Precision: " + str(len(positive_coverage)/len(coverage)))
+  print("Cumulative Recall: " + str(len(positive_coverage)/len(positive_points)))
+  print("Cumulative Coverage: " + str(len(coverage)/len(y_pred)))
+  fidelity, fidelity_positives, fidelity_negatives = explainer.get_fidelity(use_top = i + 1)
+  print("Cumulative Fidelity: ")
   print("Total: " + str(fidelity) + ", Positive: " + str(fidelity_positives) + ", Negative: " + str(fidelity_negatives))
-  print("Unexplained Positives")
-  print(len(list(set(positive_points).difference(set(explained_positive_points)))))
-
-
-  # Join
-  for stage in range(1, num_trees):
-    old_candidates = candidates
-    candidates = []
-    for i in range(len(old_candidates)):
-      for j in range(i, len(old_candidates)):
-        r = old_candidates[i].join(old_candidates[j])
-        if(r is not None):
-          min_score, max_score = random_forest.get_rule_score(r.decision_rule)
-          
-          decision_rule_precision = 0.00
-          if(min_score*learning_rate + bias >= 0):
-            decision_rule_precision = 1.00
-
-          solution_is_possible = True 
-          if(max_score*learning_rate + bias < 0):
-            solution_is_possible = False
-                  
-          decision_value = []
-          for data_index in r.decision_support:
-            decision_value.append(y_pred[data_index])
-          decision_rule_precision = sum(decision_value)/len(decision_value)
-          
-          if(max(decision_value) == 0):
-            solution_is_possible = False
-          
-          decision_support_positive = list(set(r.decision_support).intersection(set(positive_points)))
-          
-          if(len(list(set(decision_support_positive).difference(set(explained_positive_points)))) == 0):
-            solution_is_possible = False
-          
-          if(solution_is_possible):
-            if(decision_rule_precision >= fidelity_threshold):
-              solutions.append(r)
-              explained_positive_points = list(set(explained_positive_points).union(set(decision_support_positive)))
-            else:
-              candidates.append(r)
-
-    print()
-    print("Stage " + str(stage))
-    print("Pruning Candidates")
-    pruned_candidates = []
-    for i in range(len(candidates)):
-      r = candidates[i]
-      decision_support_positive = list(set(r.decision_support).intersection(set(positive_points)))
-      
-      solution_is_possible = True
-      if(len(list(set(decision_support_positive).difference(set(explained_positive_points)))) == 0):
-        solution_is_possible = False
-      
-      if(solution_is_possible):
-        pruned_candidates.append(r)
-    candidates = pruned_candidates
-    
-    print()
-    print(str(len(candidates)) + " candidates")
-    print(str(len(solutions)) + " solutions")
-    print("Merging candidates and solutions...")
-    candidates = merge(candidates)
-    solutions = merge(solutions)
-    print(str(len(candidates)) + " candidates")
-    print(str(len(solutions)) + " solutions")
-    print()
-    fidelity, fidelity_positives, fidelity_negatives = get_fidelity(solutions, y_pred)
-    print("Fidelity")
-    print("Total: " + str(fidelity) + ", Positive: " + str(fidelity_positives) + ", Negative: " + str(fidelity_negatives))
-    print("Unexplained Positives")
-    print(len(list(set(positive_points).difference(set(explained_positive_points)))))
-
-
   print()
-  print("Set Cover")
-
-  rules = [] 
-  positive_support = []
-  for r in solutions:
-    rules.append(r.decision_rule)
-    positive_support.append(list(set(positive_points).intersection(set(r.decision_support))))
-
-  covered_rules = []
-  while(True):
-    #choose biggest one
-    index = 0
-    while((index < len(positive_support)) and (len(positive_support[index]) == 0)):
-      index = index + 1
-    if(index == len(positive_support)):
-      break
-
-    for i in range(len(positive_support)):
-      if(len(positive_support[i]) > 0):
-        if(len(positive_support[i]) > len(positive_support[index])):
-          index = i
-        else:
-          if(len(positive_support[i]) == len(positive_support[index])):
-            if(len(rules[i]) < len(rules[index])):
-              index = i
-
-    covered_rules.append(index)
-
-    #remove support
-    for i in range(len(positive_support)):
-      if(index != i):
-        positive_support[i] = list(set(positive_support[i]).difference(set(positive_support[index])))
-    positive_support[index] = []
-
-  old_solutions = solutions
-  solutions = []
-  for i in covered_rules:
-    solutions.append(old_solutions[i])
-
-  rule_precision_list, rule_recall_list, rule_coverage_list = [], [], []
-
-  print()
-  print("Solutions:")
-  coverage = []
-  positive_coverage = []
-  for i in range(len(solutions)):
-    r = solutions[i]
-    print("Rule " + str(i + 1) + ": " + str(r.decision_rule))
-    positive_support = list(set(positive_points).intersection(set(r.decision_support)))
-    # rule_precision
-    rule_precision = len(positive_support)/len(r.decision_support)
-    rule_precision_list.append(rule_precision)
-    print("Precision: " + str(rule_precision))
-    # rule_recall
-    rule_recall = len(positive_support)/len(positive_points)
-    rule_recall_list.append(rule_recall)
-    print("Recall: " + str(rule_recall))
-    # rule_coverage
-    rule_coverage = len(r.decision_support)/len(y_pred)
-    rule_coverage_list.append(rule_coverage)
-    print("Coverage: " + str(rule_coverage))
-    coverage = list(set(coverage).union(r.decision_support))
-    positive_coverage = list(set(positive_coverage).union(positive_support))
-    print("Cumulative Precision: " + str(len(positive_coverage)/len(coverage)))
-    print("Cumulative Recall: " + str(len(positive_coverage)/len(positive_points)))
-    print("Cumulative Coverage: " + str(len(coverage)/len(y_pred)))
-    fidelity, fidelity_positives, fidelity_negatives = get_fidelity(solutions[0 : i+1], y_pred)
-    print("Cumulative Fidelity: ")
-    print("Total: " + str(fidelity) + ", Positive: " + str(fidelity_positives) + ", Negative: " + str(fidelity_negatives))
-    print()
 
 
-  print()
-  print(str(num_trees) + " trees")
-  print(str(len(solutions)) + " solutions")
-  fidelity, fidelity_positives, fidelity_negatives = get_fidelity(solutions, y_pred)
-  print("Fidelity")
-  print("Total: " + str(fidelity) + ", Positive: " + str(fidelity_positives) + ", Negative: " + str(fidelity_negatives))
-  
-  return solutions, trainer, rule_precision_list, rule_recall_list, rule_coverage_list
+print()
+print(str(num_trees) + " trees")
+print(str(len(solutions)) + " solutions")
+fidelity, fidelity_positives, fidelity_negatives = explainer.get_fidelity()
+print("Fidelity")
+print("Total: " + str(fidelity) + ", Positive: " + str(fidelity_positives) + ", Negative: " + str(fidelity_negatives))
+
+
