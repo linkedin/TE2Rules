aaf6127a20c59506284ca53b4e2565da21a7a957
G Roshan Lal <groshanlal@gmail.com>
2022-05-05 21:29:26 -0700

    Adding data_prep for other datasets: breast-cancer, contraception, spambase, heart, mushroom

---
+++ data_prep_breast.py
@@ -0,0 +1,56 @@
+from data_prep import *
+from sklearn.utils import shuffle
+import numpy as np
+
+np.random.seed(123)
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer/"
+download_to = 'data/'
+files = ['breast-cancer.data']
+column_names = [
+'label',
+'age',
+'menopause',
+'tumor_size',
+'inv_nodes',
+'node_caps',
+'deg_malig',
+'breast',
+'breast_quad',
+'irradiat',
+]
+column_names_categorical = [
+'age',
+'menopause',
+'tumor_size',
+'inv_nodes',
+'node_caps',
+'deg_malig',
+'breast',
+'breast_quad',
+'irradiat',
+'label'
+]
+
+download(url, download_to, files)
+
+df = clean_missing(download_to + 'breast-cancer.data', column_names, sep = ",")
+df = shuffle(df)
+df = rename_label(df, 'label', ['no_recurrence_events', 'recurrence_events'], ['0', '1'])
+
+num_rows_train = int(0.8*len(df))
+
+df_train = df[:num_rows_train]
+df_test = df[num_rows_train:]
+df_train.to_csv('data/train_raw.csv', index = False)
+df_test.to_csv('data/test_raw.csv', index = False)
+
+df = pd.get_dummies(data=df, columns=column_names_categorical)
+df = df.drop('label_0', 1)
+
+df_train = df[:num_rows_train]
+df_test = df[num_rows_train:]
+df_train.to_csv('data/train.csv', index = False)
+df_test.to_csv('data/test.csv', index = False)
+
+
