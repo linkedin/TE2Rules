51177f1cb23b76bfbd5e5151d6aa4958d1de36aa
G Roshan Lal <groshanlal@gmail.com>
2022-09-18 03:16:33 -0700

    Adding tests for adapter

---
+++ tests/test_adapter.py
@@ -0,0 +1,74 @@
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
+
+from te2rules.adapter import ScikitGradientBoostingClassifierAdapter
+
+np.random.seed(42)
+
+x_train = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 1, 0, 0],
+    [0, 1, 0, 1],
+    [0, 1, 1, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 1],
+    [1, 0, 1, 0],
+    [1, 0, 1, 1],
+    [1, 1, 0, 0],
+    [1, 1, 0, 1],
+    [1, 1, 1, 0],
+]
+y_train = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+feature_names = ["f0", "f1", "f2", "f3"]
+
+learning_rate = 0.1
+num_positive = sum(y_train)
+num_negative = len(y_train) - sum(y_train)
+
+num_trees = 3
+model = GradientBoostingClassifier(
+    n_estimators=num_trees, max_depth=2, learning_rate=learning_rate
+)
+model.fit(x_train, y_train)
+model_scores = model.predict_proba(x_train)[:, 1]
+
+adapted_model = ScikitGradientBoostingClassifierAdapter(
+    model, feature_names
+).random_forest
+
+rules = adapted_model.get_rules(x_train)
+adapted_model_scores = adapted_model.get_scores()
+
+
+def test_num():
+    assert adapted_model.get_num_trees() == num_trees
+
+
+def test_weights_and_bias():
+    weight = adapted_model.weight
+    bias = adapted_model.bias
+    assert weight == learning_rate
+    assert bias == np.log(num_positive / num_negative)
+
+
+def test_activation():
+    assert adapted_model.activation == "sigmoid"
+
+
+def test_scores():
+    assert len(model_scores) == len(adapted_model_scores)
+    for i in range(len(model_scores)):
+        assert abs(model_scores[i] - adapted_model_scores[i]) < 10 ** (-6)
