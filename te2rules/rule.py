950520339ba42135c9b620c61e054261ee3c98e4
G Roshan Lal <groshanlal@gmail.com>
2022-09-18 20:57:58 -0700

    Adding type annotations to rule.py

--- te2rules/rule.py
+++ te2rules/rule.py
@@ -1,5 +1,12 @@
+from __future__ import annotations
+
+from typing import Dict, List, Optional
+
+
 class Rule:
-    def __init__(self, decision_rule, decision_support, identity):
+    def __init__(
+        self, decision_rule: List[str], decision_support: List[int], identity: List[str]
+    ):
         self.decision_rule = decision_rule
         self.decision_support = decision_support
         self.identity = identity
@@ -10,11 +17,11 @@ class Rule:
                 + "that produce this rule. Identity list cannot be empty"
             )
 
-    def __str__(self):
+    def __str__(self) -> str:
         string_rep = " & ".join(self.decision_rule)
         return string_rep
 
-    def create_identity_map(self):
+    def create_identity_map(self) -> None:
         num_nodes = len(self.identity[0].split(","))
         for i in range(len(self.identity)):
             node_ids = self.identity[i].split(",")
@@ -25,8 +32,8 @@ class Rule:
                     + "unequal number of contributing tree nodes."
                 )
 
-        left_identity_map = {}
-        right_identity_map = {}
+        left_identity_map: Dict[str, List[str]] = {}
+        right_identity_map: Dict[str, List[str]] = {}
         for i in range(len(self.identity)):
             node_ids = self.identity[i].split(",")
 
@@ -44,7 +51,7 @@ class Rule:
         self.right_identity_map = right_identity_map
         return
 
-    def join_identity(self, rule):
+    def join_identity(self, rule: Rule) -> List[str]:
         if not hasattr(self, "left_identity_map") or not hasattr(
             rule, "right_identity_map"
         ):
@@ -84,29 +91,29 @@ class Rule:
 
         return list(set(joined_identity))
 
-    def validate_identity(self, joined_identity):
+    def validate_identity(self, joined_identity: List[str]) -> bool:
         if len(joined_identity) > 0:
             return True
         else:
             return False
 
-    def join_rule(self, rule):
+    def join_rule(self, rule: Rule) -> List[str]:
         decision_rule = list(set(self.decision_rule).union(set(rule.decision_rule)))
         return sorted(decision_rule)
 
-    def join_support(self, rule):
+    def join_support(self, rule: Rule) -> List[int]:
         decision_support = list(
             set(self.decision_support).intersection(set(rule.decision_support))
         )
         return decision_support
 
-    def validate_support(self, joined_support):
+    def validate_support(self, joined_support: List[int]) -> bool:
         if len(joined_support) > 0:
             return True
         else:
             return False
 
-    def join(self, rule, support_pruning=False):
+    def join(self, rule: Rule, support_pruning: bool = False) -> Optional[Rule]:
         decision_rule = self.join_rule(rule)
 
         decision_support = self.join_support(rule)
